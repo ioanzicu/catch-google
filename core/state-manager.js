37213da69160657752da5183d118f12ee6981cd5
ioan.zicu <ioan.zicu@olx.pl>
2024-09-03 23:33:38 +0200

    added google and player img components

--- core/state-manager.js
+++ core/state-manager.js
@@ -5,12 +5,39 @@ const _state = {
             columns: 4,
         },
     },
+    positions: {
+        google: {
+            row: 1, 
+            col: 1,
+        },
+        players: [
+            {
+                row: 1, 
+                col: 0,
+            },
+            {
+                row: 3, 
+                col: 2,
+            },
+        ]
+    },
     points: {
         google: 12,
         players: [3, 7]
     }
 }
 
+function _getPlayerIndexByNumber(playerNumber) {
+    const idx = playerNumber - 1
+    
+    if (idx < 0 || idx > _state.points.players.length - 1) {
+        throw new Error("incorrect player number")
+    }
+
+    return idx
+}
+
+// INTERFACE/ADAPTER
 export async function getGooglePoints() {
     return _state.points.google
 }
@@ -21,12 +48,7 @@ export async function getGooglePoints() {
  * @returns {Promise<number>} number of points
  */
 export async function getPlayerPoints(playerNumber) {
-    const idx = playerNumber - 1
-    
-    if (idx < 0 || idx > _state.points.players.length - 1) {
-        throw new Error("incorrect player number")
-    }
-
+    const idx = _getPlayerIndexByNumber(playerNumber)
     return _state.points.players[idx]
 }
 
@@ -34,4 +56,13 @@ export async function getPlayerPoints(playerNumber) {
 export async function getGridSize() {
     // copy of setting, to not allow mutability
     return {..._state.settings.gridSize}
+}
+
+export async function getGooglePosition() {
+    return {..._state.positions.google}
+}
+
+export async function getPlayerPosition(playerNumber) {
+    const idx = _getPlayerIndexByNumber(playerNumber)
+    return {..._state.positions.players[idx]}
 }
--- ui/components/Grid/Cell.js
+++ ui/components/Grid/Cell.js
@@ -1,3 +1,8 @@
+import { getGooglePosition, getPlayerPosition } from "../../../core/state-manager.js"
+import { GoogleComponent } from "../common/Google/GoogleComponent.js"
+import { PlayerComponent } from "../common/Player/PlayerComponent.js"
+
+
 export function Cell(row, col) {
     const element = document.createElement('td')
     render(element, row, col)
@@ -5,5 +10,21 @@ export function Cell(row, col) {
 }
 
 async function render(element, row, col) {
-    element.append(`${row} ${col}`)
+
+    const googlePosition = await getGooglePosition()
+    const player1Position = await getPlayerPosition(1)
+    const player2Position = await getPlayerPosition(2)
+
+    if (googlePosition.row === row && googlePosition.col === col) {
+        element.append(GoogleComponent().element)
+    }
+
+
+    if (player1Position.row === row && player1Position.col === col) {
+        element.append(PlayerComponent(1).element)
+    }
+
+    if (player2Position.row === row && player2Position.col === col) {
+        element.append(PlayerComponent(2).element)
+    }
 }
